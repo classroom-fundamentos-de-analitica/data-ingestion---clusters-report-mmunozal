d2869f5aa8dc5942f3404ef99470fea168c9f372
mmunozal <55594437+mmunozal@users.noreply.github.com>
2022-10-10 20:17:22 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,29 @@ espacio entre palabra y palabra.
 
 
 """
-import pandas as pd
 
+import pandas as pd
+import numpy as np
+import math
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
-    return df
+  archivo=pd.read_fwf(
+    "clusters_report.txt",
+    colspecs="infer",
+    widths=[8,10,20,100],
+  )
+  datos=archivo.drop([0,1],axis=0)
+  datos = pd.DataFrame(datos)
+  cluster=datos.iloc[:,0]
+  palabras=datos.iloc[:,1]
+  porcentaje=datos.iloc[:,2]
+  cluster = cluster.dropna()
+  palabras = palabras.dropna()
+  porcentaje = porcentaje.dropna()
+  df = pd.DataFrame(columns=['cluster','cantidad de palabras clave','porcentaje de palabras clave','principales palabras clave'])
+  for i in range(0,13):
+    df = df.append({'cluster': cluster.iloc[i]}, ignore_index=True)
+    df['cantidad de palabras clave'].fillna(palabras.iloc[i],inplace=True)
+    df['porcentaje de palabras clave'].fillna(porcentaje.iloc[i],inplace=True)
+  final=df
+  return final
