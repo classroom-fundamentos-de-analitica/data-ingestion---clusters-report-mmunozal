d7e2beb87ec6405679900096df11e2a42368f0a7
mmunozal <55594437+mmunozal@users.noreply.github.com>
2022-10-16 16:54:32 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,25 +17,25 @@ import math
 def ingest_data():
 
     df = pd.read_fwf(
-        "clusters_report.txt", widths = [9, 16, 16, 80], header = None,
+        "clusters_report.txt", widths = [8,10,20,100], header = None,
         names = ["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave", "-"],
         skip_blank_lines = False, converters = {"porcentaje_de_palabras_clave": 
         lambda x: x.rstrip(" %").replace(",",".")}).drop([0,1,2,3], axis=0)
 
-    col4 = df["-"]
+    porcentaje = df["-"]
     df = df[df["cluster"].notna()].drop("-", axis=1)
     df = df.astype({ "cluster": int, "cantidad_de_palabras_clave": int, "porcentaje_de_palabras_clave": float})
 
-    col4Proc = []
+    porcentaje_total = []
     text = ""
-    for lin in col4:
+    for lin in porcentaje:
         if isinstance(lin, str): text += lin+" "
         else:
             text = ", ".join([" ".join(x.split()) for x in text.split(",")])
-            col4Proc.append(text.rstrip("."))
+            porcentaje_total.append(text.rstrip("."))
             text = ""
             continue
 
-    df["principales_palabras_clave"] = col4Proc
+    df["principales_palabras_clave"] = porcentaje_total
 
     return df
